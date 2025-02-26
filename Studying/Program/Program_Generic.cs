7fbd269ca9d981b8dad9ea87d6506feba802a05f
ImNotHerForToday <edgarasvalentynukas@gmail.com>
2025-02-26 10:22:28 +0200

    Completed task Generics

---
+++ Studying/Program/Program_Generic.cs
@@ -0,0 +1,42 @@
+﻿using Studying.Tasks.Task_Loops_Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying.Program
+{
+    class Program_Generic
+    {
+        static void Main(string[] args)
+        {
+            GenericArray<int> intArray = new GenericArray<int>();
+
+            intArray.AddItem(5);
+            intArray.AddItem(10);
+            intArray.AddItem(15);
+            intArray.AddItem(20);
+            intArray.AddItem(25);
+            intArray.AddItem(30);
+
+            Console.WriteLine("\nItems:");
+            intArray.DisplayItems();
+
+            intArray.RemoveIndex(1);
+
+            Console.WriteLine("\nItems:");
+            intArray.DisplayItems();
+
+            Console.WriteLine("");
+            intArray.GetSpecificIndex(3);
+
+            Console.WriteLine("\nCount:");
+            intArray.GetCount();
+
+           
+        }
+
+
+    }
+}
