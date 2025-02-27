214307be79423f88bacce9748fb71f33aff86f49
ImNotHerForToday <edgarasvalentynukas@gmail.com>
2025-02-27 11:55:54 +0200

    Task 10 + fixed structure

---
+++ Studying/Program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task Construct if-else-switch
+            //Program_Calculations calculations = new Program_Calculations();
+            //calculations.RunCalculationTask_Constructs_If_Else_Switch();
+
+
+
+            ////Task Loops and Arrays
+            //Program_LoopsArrays loopsArrays = new Program_LoopsArrays();
+            //loopsArrays.RunLoopsArraysTask();
+
+
+
+            ////Task Generic
+            //Program_Generic generic = new Program_Generic();
+            //generic.RunGenericArrayTask_Generics();
+
+
+
+            //Task Collections
+            Program_Collections collections = new Program_Collections();
+            //collections.RunListTask_Collections();
+            //collections.RunLinkedListTask_Collections();
+            //collections.RunQueueAndStack_Collections();
+            //collections.RunDictionary_Collections();
+            //collections.RunDictionary_Collections_Task2();
+            collections.RunDictionary_Collections_Task3();
+
+
+
+        }
+    }
+}
---
+++ Studying/Program/Program_Calculations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Studying.Tasks.Task_Collections.Collections;
+
+namespace Studying.Program
+{
+    class Program_Calculations : Calculations
+    {
+        public void RunCalculationTask_Constructs_If_Else_Switch()
+        {
+            Calculations Calculation = new Calculations();
+            Calculation.RunCalculations();
+        }
+    }
+}
